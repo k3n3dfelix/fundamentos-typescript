92564fba7de41732ded197bd3f09c48674f85657
Kened Silva Felix <ksfelix1@stefanini.com>
2022-10-11 20:47:06 -0300

    Avançando em tipagens e iniciado narrowing

---
+++ 4-Narrowing/index.js
@@ -0,0 +1,15 @@
+//1 - type guard
+function sum(a, b) {
+    if (typeof a === "string" && typeof b === "string") {
+        console.log(parseFloat(a) + parseFloat(b));
+    }
+    else if (typeof a === "number" && typeof b === "number") {
+        console.log(a + b);
+    }
+    else {
+        console.log("Impossivel realizar a soma!");
+    }
+}
+sum("4", "59");
+sum(12, 12.45);
+sum("5", 6);
